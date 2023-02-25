78fafbf69d1a4bce4bba0711f971f4c429a48471
Rolando Santamaria Maso <kyberneees@gmail.com>
2023-02-25 18:46:39 +0100

    linting

--- demos/minimal.ts
+++ demos/minimal.ts
@@ -4,7 +4,6 @@ import Redis from "ioredis"
 const redis = new Redis({ host: "localhost", port: 6379 })
 const repository = new SortedItemRepository("my-sorted-repo", redis)
 
-
 async function run() {
   await repository.deleteAll()
 
@@ -20,5 +19,3 @@ async function run() {
 }
 
 run().catch(console.log)
-
-
